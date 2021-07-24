ba0bcd38e2df01a11333a109bfbd0accc763d781
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-07-24 14:59:26 -0500

    firebase-service.service.ts works as a main way to communicate with database by its generic methods.

---
+++ src/app/services/firebase-service.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/firestore";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseServiceService {
+
+  constructor(private firebase: AngularFirestore) { }
+
+  public addDocument(collection: string, data: any){
+    return this.firebase.collection(collection).add(data);
+  }
+
+  public getDocuments(collection: string){
+    return this.firebase.collection(collection).snapshotChanges();
+  }
+
+  public async getById(collection: string, docID: string){
+    return await this.firebase.collection(collection).doc(docID).get().toPromise();
+  }
+
+  public async getByField(collection: string, field: string, fieldValue: string){
+    return await this.firebase.collection(collection, ref => ref.where(field, '==', fieldValue)).get()
+      .toPromise();
+  }
+
+  public update(collection: string, docID: string, data: any){
+    return this.firebase.collection(collection).doc(docID).set(data);
+  }
+
+  public delete(collection: string, docID: string){
+    return this.firebase.collection(collection).doc(docID).delete();
+  }
+
+}
