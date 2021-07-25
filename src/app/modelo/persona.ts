ea88a62237b85ab1b1db54284b95c00175ae75be
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-07-24 21:22:59 -0500

    Ignore this commit.

--- src/app/modelo/persona.ts
+++ src/app/modelo/persona.ts
@@ -1,4 +1,5 @@
 export class Persona{
+  doc_id: string
   Codigo: string;
   Nombres: string;
   Correo: string;
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,7 +4,7 @@ import {AngularFirestore} from "@angular/fire/firestore";
 @Injectable({
   providedIn: 'root'
 })
-export class FirebaseServiceService {
+export class FirebaseService {
 
   constructor(private firebase: AngularFirestore) { }
 
